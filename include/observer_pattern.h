538bcfa22bcfb1b1add5bebb0309be25a8f884a9
doanhuynh049 <quocthien049@gmail.com>
2023-07-09 15:55:16 +0700

    update

--- include/observer_pattern.h
+++ include/observer_pattern.h
@@ -2,6 +2,7 @@
 #define OBSERVER_PATTERN_H
 #include <string>
 #include <iostream>
+#include <list>
 
 class IObserver
 {
@@ -17,7 +18,97 @@ class ISubject
     virtual void Detach(IObserver *observer) = 0;
     virtual void Notify() = 0;
 };
+class Subject : public ISubject
+{
+   public:
+    virtual ~Subject() { std::cout << "Goodbye, I was the Subject.\n"; }
+    void Attach(IObserver *observer) override;
+    void Detach(IObserver *observer) override;
+    void Notify();
+    // void HowManyObserver();
+    void CreateMessage(std::string message = "Empty");
 
+   private:
+    std::list<IObserver *> list_observer_;
+    std::string message_;
+};
+class Observer : public IObserver
+{
+   public:
+    Observer(Subject &subject);
+    virtual ~Observer()
+    {
+        std::cout << "Goodbye, I was the Observer \"" << this->number_
+                  << "\".\n";
+    }
+    void Update(const std::string &message_from_subject) override;
+    void RemoveMeFromTheList();
+    void PrintInfor();
+
+   private:
+    std::string message_from_subject_;
+    Subject &subject_;
+    static int static_number_;
+    int number_;
+};
 void ClientCode();
 
+// build a weather monitoring application
+// class WeatherData : public ISubject
+// {
+//    public:
+//     virtual ~WeatherData() { std::cout << "Goodby, I was the WeatherData.\n";
+//     } void Attach(IObserver *observer) override; void Detach(IObserver
+//     *observer) override; void Notify() override; void setTemperature(float
+//     temperature); void setHumidity(float humidity);
+
+//    private:
+//     float temperature_;
+//     float humidity_;
+//     std::list<IObserver *> observer_;
+//     std::string message_;
+// };
+// class TemperatureDisplay : public IObserver
+// {
+//    public:
+//     TemperatureDisplay(WeatherData *weather_data, bool is_fahrenheit)
+//         : weather_data_(weather_data), is_fahrenheit_(is_fahrenheit)
+//     {
+//     }
+
+//     void Update1() override;
+
+//    private:
+//     WeatherData *weather_data_;
+//     bool is_fahrenheit_;
+// };
+
+// // class HumidityDisplay : public IObserver
+// // {
+// //    public:
+// //     HumidityDisplay(WeatherData *weatherdata) : weather_data_(weatherdata)
+// {}
+// //     void Update1();
+
+// //    private:
+// //     std::string message_from_subject_;
+
+// //     WeatherData *weather_data_;
+// // };
+// // class Alarm : public IObserver
+// // {
+// //    public:
+// //     Alarm(WeatherData *weatherdata, float threshold)
+// //         : weather_data_(weatherdata), threshold_(threshold)
+// //     {
+// //     }
+// //     void Update();
+
+// //    private:
+// //     WeatherData *weather_data_;
+// //     std::string message_from_subject_;
+// //     float threshold_;
+// // };
+
+// void ClientCodeWeather();
 #endif
--- src/obsever_pattern.cpp
+++ src/obsever_pattern.cpp
@@ -1,82 +1,55 @@
-#include <iostream>
-#include <list>
-#include <string>
+
 #include "../include/observer_pattern.h"
 
-class Subject : public ISubject
+class Observer;
+class ISubject;
+void Subject::Attach(IObserver *observer)
 {
-   public:
-    virtual ~Subject() { std::cout << "Goodbye, I was the Subject.\n"; }
-    void Attach(IObserver *observer) override
-    {
-        list_observer_.push_back(observer);
-    }
-    void Detach(IObserver *observer) override
-    {
-        list_observer_.remove(observer);
-    }
-    void Notify()
-    {
-        std::list<IObserver *>::iterator iterator = list_observer_.begin();
-        HowManyObserver();
-        while (iterator != list_observer_.end())
-        {
-            (*iterator)->Update(message_);
-            ++iterator;
-        }
-    }
-    void HowManyObserver()
-    {
-        std::cout << "There are " << list_observer_.size()
-                  << " observers in the list.\n";
-    }
-    void CreateMessage(std::string message = "Empty")
-    {
-        this->message_ = message;
-        Notify();
-    }
-
-   private:
-    std::list<IObserver *> list_observer_;
-    std::string message_;
-};
-class Observer : public IObserver
+    list_observer_.push_back(observer);
+}
+void Subject::Detach(IObserver *observer) { list_observer_.remove(observer); }
+void Subject::Notify()
 {
-   public:
-    Observer(Subject &subject) : subject_(subject)
+    std::list<IObserver *>::iterator iterator = list_observer_.begin();
+    std::cout << "There are " << list_observer_.size()
+              << " observers in the list.\n";
+    while (iterator != list_observer_.end())
     {
-        this->subject_.Attach(this);
-        std::cout << "Hi, I'm Observer \" " << ++Observer::static_number_
-                  << "\".\n";
-        this->number_ = Observer::static_number_;
+        (*iterator)->Update(message_);
+        ++iterator;
     }
-    virtual ~Observer()
-    {
-        std::cout << "Goodbye, I was the Observer \"" << this->number_
-                  << "\".\n";
-    }
-    void Update(const std::string &message_from_subject) override
-    {
-        message_from_subject_ = message_from_subject;
-    }
-    void RemoveMeFromTheList()
-    {
-        subject_.Detach(this);
-        std::cout << "Observer \"" << number_ << "\"removed from the list/\n";
-    }
-    void PrintInfor()
-    {
-        std::cout << "Observer \" " << this->number_
-                  << "\" : a new message is available" << this->>
-            message_from_subject_;
-    }
+}
 
-   private:
-    std::string message_from_subject_;
-    Subject &subject_;
-    static int static_number_;
-    int number_;
-};
+void Subject::CreateMessage(std::string message)
+{
+    this->message_ = message;
+    Notify();
+}
+
+Observer::Observer(Subject &subject) : subject_(subject)
+{
+    this->subject_.Attach(this);
+    std::cout << "Hi, I'm Observer \" " << ++Observer::static_number_
+              << "\".\n";
+    this->number_ = Observer::static_number_;
+}
+
+void Observer::Update(const std::string &message_from_subject)
+{
+    message_from_subject_ = message_from_subject;
+    PrintInfor();
+}
+void Observer::RemoveMeFromTheList()
+{
+    subject_.Detach(this);
+    std::cout << "Observer \"" << number_ << "\"removed from the list /\n ";
+}
+void Observer::PrintInfor()
+{
+    std::cout << "Observer \" " << this->number_
+              << "\" : a new message is available --> "
+              << this->message_from_subject_ << std::endl;
+}
 
 int Observer::static_number_ = 0;
 
@@ -90,5 +63,57 @@ void ClientCode()
     Observer *observer5;
 
     subject->CreateMessage("Hello world!");
-    obser
+    observer3->RemoveMeFromTheList();
+
+    subject->CreateMessage("The weather is hot today! :p");
+    observer4 = new Observer(*subject);
+
+    observer2->RemoveMeFromTheList();
+    observer5 = new Observer(*subject);
+
+    subject->CreateMessage("My new car is great! ");
+    observer5->RemoveMeFromTheList();
+    observer1->RemoveMeFromTheList();
+    observer4->RemoveMeFromTheList();
+
+    delete observer1;
+    delete observer2;
+    delete observer3;
+    delete observer4;
+    delete observer5;
 }
+
+// void WeatherData::Attach(IObserver *observer) {
+// observer_.push_back(observer); } void WeatherData::Detach(IObserver
+// *observer) { observer_.remove(observer); } void WeatherData::Notify()
+// {
+//     std::list<IObserver *>::iterator iterator = observer_.begin();
+//     // std::cout << "There are " << observer_.size() << " observers in the
+//     // list\n";
+//     std::cout << this->message_;
+//     while (iterator != observer_.end())
+//     {
+//         (*iterator)->Update1();
+//         iterator++;
+//     }
+// }
+// void WeatherData::setTemperature(float temperature)
+// {
+//     temperature_ = temperature;
+//     this->message_ = "Temperature is changed\n";
+//     Notify();
+// }
+// void WeatherData::setHumidity(float humidity)
+// {
+//     humidity_ = humidity;
+//     this->message_ = "Humidity is changed\n";
+//     Notify();
+// }
+// // void TemperatureDisplay::Update1() { ; }
+// void ClientCodeWeather()
+// {
+//     WeatherData *weatherdata = new WeatherData();
+//     weatherdata->setTemperature(12);
+//     weatherdata->setHumidity(50);
+//     TemperatureDisplay temperature_celsius(weatherdata, false);
+// }
